2fcefc9b3923c03d5b0bcc40378b587a4a36ba78
josefallas0302 <jafp0302@gmail.com>
2018-05-27 18:25:52 -0600

    Clase 4 no terminada aun

---
+++ Clase#1/HolaMundo.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+
+# Filename : mundo.py
+# Author :   Cristian Gutiérrez
+
+# Description: This file print two lines. This is the first example of "Introducción a Python"
+
+print("Hola Mundo\n")
+print("Este mensaje fue leído desde un archivo con extensión .py")
+
+a = 3
+b = 6
+c = a * b
+print('El resultado de multiplicar {} por {} es: {}'.format(a, b, c))
+print('El resultado de multiplicar ' + str(a) + ' por ' + str(b) + ' es: ' + str(c))
---
+++ Clase#1/input.py
@@ -0,0 +1,6 @@
+edad = int(input("¿Cuántos años tiene? "))
+if edad < 18:
+    print("Es usted menor de edad")
+else:
+    print("Es usted mayor de edad")
+print("¡Hasta la próxima!")
