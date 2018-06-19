0943bcc3337df1bfa8e98a22062b5b6cb73cda79
josefallas0302 <jafp0302@gmail.com>
2018-06-19 10:45:25 -0600

    Clase 2 Cuadratica

--- Clase#2/cuadratica.py
+++ Clase#2/cuadratica.py
@@ -4,3 +4,7 @@ a = int(input("Digite el valor de a "))
 b = int(input("Digite el valor de b "))
 c = int(input("Digite el valor de c "))
 
+## Calcular el discriminante a partir de Los argumentosK
+K = a+b
+d = math.sqrt(K)
+print("Valor de K es:", K,"Valor de la raiz:",d)
