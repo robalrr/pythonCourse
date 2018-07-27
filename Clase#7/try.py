8e9afd95841098617806e58d35d15edccbb22ce9
Cristian Gutierrez <cristian@debian>
2018-07-27 00:52:58 -0600

    Documentos de la clase 7

---
+++ Clase#7/try.py
@@ -0,0 +1,12 @@
+from goto import goto, label
+
+dividendo = int(input("dividendo: "))
+label .divisor
+divisor = int(input("divisor: "))
+
+try:
+    cociente = dividendo / divisor
+
+except:
+    print("No se permite la division por cero")
+    goto .divisor
